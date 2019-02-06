3ec45b69767bebef35f85bb9f4a76602ea6c0cd0
JazHar <jasonharris06@gmail.com>
2019-02-05 22:44:23 -0700

    database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,40 @@
 var mySQL = require("mysql");
 var inquirer = require("inquirer");
 var keys = require("./keys");
+var consoleTable = require("console.table");
+
+var connection = mySQL.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "34XYkf*5",
+    database: "bamazonCustomer_DB"
 
 
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    displayItems();
+    //runSearch();
+});
+
+// function runSearch() {
+//     inquirer
+//     .prompt({
+
+//     })
+// };
+
+function displayItems(){
+    
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.table(res);
+        // console.log("-----------------------------------");
+        // console.log(res.id + " | " + res.Product_Name + " | " + res.Department_Name + " | " + res.Price + " | " + res.Stock_Quantity );
+        // for (var i = 0; i < res.length; i++) {
+        //   console.log(res[i].id + " | " + res[i].Product_Name + " | " + res[i].Department_Name + " | "  + res[i].Price + " | " + res[i].Stock_Quantity);
+        // }
+        // console.log("-----------------------------------");
+      });
+}
